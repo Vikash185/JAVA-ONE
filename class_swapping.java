47a74ab84ea6e09eda4cc6c41a19e95131b4c226
ViCcK <vksiwan456@gmail.com>
2024-09-21 23:35:33 +0530

    21-09-2024

---
+++ class_swapping.java
@@ -0,0 +1,11 @@
+public class class_swapping {
+    public static void main(String[] args) {
+        int a=5;
+        int b=8;
+        int c=a;
+    a=b;
+    b=c;
+    System.out.println("a has"+a);
+    System.out.println("b has"+b);
+    }
+}
